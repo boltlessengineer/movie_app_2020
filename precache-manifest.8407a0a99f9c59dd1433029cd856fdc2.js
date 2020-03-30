06cc6ddee229824e8adc39c9ff07f813e68dbd8a
Seongmin0801 <seongmin0801@gmail.com>
2020-03-30 18:56:59 +0900

    Updates

---
+++ precache-manifest.8407a0a99f9c59dd1433029cd856fdc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3edd238f45245b02fa2e3477ad8a8a3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "8ae3d7aef180593d9c6e",
+    "url": "/movie_app_2020/static/css/main.b7cf6848.chunk.css"
+  },
+  {
+    "revision": "391adac8bd45a1e1de19",
+    "url": "/movie_app_2020/static/js/2.7cbcc22b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.7cbcc22b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae3d7aef180593d9c6e",
+    "url": "/movie_app_2020/static/js/main.06cf44fc.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
